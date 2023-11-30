2ae509bc4371c2070250ab757a918e38c8175a08
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-30 15:25:30 +0300

    assembler to base 2 from base 10 challenge completed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import com.workintech.StackAndQueue.Palindrome;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-
+        /* STACK AND QUEUE CHALLENGE TESTS -> ALL PASSED
         System.out.println(checkForPalindrome("abccba"));
         System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
         System.out.println(checkForPalindrome("I did, did I?"));
@@ -23,5 +23,21 @@ public class Main {
             System.out.println("You must not enter null" );
             return false;
         }
+     */
+
+        assembler(13);
     }
+    public static void assembler(int num){
+        System.out.println("Base 10: " + num);
+        Stack<Integer> digits = new Stack<>();
+        while (0 < num) {
+            digits.push(num % 2);
+            num /= 2;
+        }
+        System.out.print("Base 2: ");
+        while(!digits.isEmpty()){
+            System.out.print(digits.pop());
+        }
+    }
+
 }
